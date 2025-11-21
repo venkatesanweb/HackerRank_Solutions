480a07f67a60296e25bfc954442efbe3da61c97d
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2025-11-21 22:11:23 +0530

    Add files via upload

---
+++ Hackerrank2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin >> a;
+    cin >>b;
+    cin >>c;
+    cin >>d;
+    cin >>e;
+    printf("%d\n",a);
+    printf("%ld\n",b);
+    printf("%c\n",c);
+    printf("%.3f\n",d);
+    printf("%.9f",e);
+    
+    return 0;
+}
